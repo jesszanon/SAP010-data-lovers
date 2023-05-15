aceb59b18e60819940290de3e4d6c8b545656ba7
Alessandra Alves <alessandraalveslopes04@gmail.com>
2023-05-15 18:46:38 -0300

    feito filtros de continentes e mudanças de css

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 // estas funciones son de ejemplo
-
+/* 
 import countries from "./data/countries/countries";
 
 export const example = () => {
@@ -9,4 +9,110 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+ */
 
+
+
+function filtroAz(arr, opcao) {
+  if (opcao == 'az'){
+    return arr.sort((a, b) => {
+      if (a.name.common < b.name.common) {
+          return -1;
+      }
+      if (a.name.common > b.name.common) {
+          return 1;
+      }
+      return 0;
+  })
+}
+
+else if(opcao == 'za'){
+  return arr.sort((a, b) => {
+    if (a.name.common > b.name.common) {
+      return -1;
+    } 
+    if (a.name.common < b.name.common) {
+      return 1;
+    } 
+      return 0;
+    });
+    } else{arr}
+  }
+
+
+  function filtroCont(arr, opcao) {
+    const america = [];
+    const europe = [];
+    const asia = [];
+    const africa = [];
+    const oceania = [];
+    const antarctica = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].continents == "America") {
+        america.push(arr[i]);
+      }
+      else if (arr[i].continents == "Europe") {
+        europe.push(arr[i]);
+      }
+      else if (arr[i].continents == "Asia") {
+        asia.push(arr[i]);
+      }else if (arr[i].continents == "Africa") {
+        africa.push(arr[i]);
+      }else if (arr[i].continents == "Oceania") {
+        oceania.push(arr[i]);
+      }
+      else  {
+        antarctica.push(arr[i]);
+      }
+  }
+  if(opcao == 'Oceania'){
+    return oceania
+  }
+  else if (opcao == 'Ásia'){
+    return asia
+  }
+  else if (opcao == 'Europa'){
+    return europe
+  }
+  else if (opcao == 'América'){
+  return america
+}
+else if (opcao == 'África'){
+  return africa
+}
+else if (opcao == 'Antártida'){
+  return antarctica
+}
+else {
+  return arr
+}
+}
+
+function exibirBandeiras(op) {
+  let exibirFlagsDiv = document.getElementById('exibirFlags');
+  exibirFlagsDiv.innerHTML = '';
+
+  let flagsCountries = op.map(country => {
+    let flagsDiv = document.createElement('div');
+    let nameElement = document.createElement('h3');
+    let flagElement = document.createElement('img');
+    flagElement.classList.add('imgBand');
+    nameElement.textContent = country.name.common;
+    flagElement.src = country.flags.png;
+
+    flagsDiv.appendChild(nameElement);
+    flagsDiv.appendChild(flagElement);
+    flagsDiv.classList.add('bands');
+
+    return flagsDiv;
+  });
+
+  flagsCountries.forEach(flag => {
+    exibirFlagsDiv.appendChild(flag);
+  });
+}
+
+
+
+export {filtroAz, filtroCont, exibirBandeiras}
--- src/main.js
+++ src/main.js
@@ -1,29 +1,18 @@
-//import { example } from './data.js';
+import { filtroAz, filtroCont, exibirBandeiras} from './data.js';
 import countries from './data/countries/countries.js';
 
-const exibirFlagsDiv = document.getElementById('exibirFlags');
-console.log(countries)
-const flagsCountries = countries.countries.map(country => {
-    const flagsDiv = document.createElement('div');
-    const nameElement = document.createElement('h2');
-    const flagElement = document.createElement('img');
+exibirBandeiras(countries.countries)
 
-    nameElement.textContent = country.name.common;
-    flagElement.src = country.flags.png;
+let filtroOp = document.getElementById('opcao')
+filtroOp.addEventListener('change', ()=>{
+   
     
+    let op = filtroOp.value
 
+ 
+exibirBandeiras(filtroCont(countries.countries, op))
 
-    flagsDiv.appendChild(nameElement);
-    flagsDiv.appendChild(flagElement);
-
-    return flagsDiv;
 });
-
-flagsCountries.forEach(flag => {
-    exibirFlagsDiv.appendChild(flag);
-});
-
-
 
 
 
